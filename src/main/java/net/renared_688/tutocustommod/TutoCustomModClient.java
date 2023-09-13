674ae806d6dc5da499bd8e7ae7828156cf97cbf5
Renared 688 <sulyvan.creton@gmail.com>
2023-09-13 22:49:17 +0200

    first commit

---
+++ src/main/java/net/renared_688/tutocustommod/TutoCustomModClient.java
@@ -0,0 +1,10 @@
+package net.renared_688.tutocustommod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutoCustomModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/renared_688/tutocustommod/TutoCustomModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.renared_688.tutocustommod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutoCustomModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
